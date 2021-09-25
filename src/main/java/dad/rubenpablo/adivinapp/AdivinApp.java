cee6aa969917bfec3f6d7417c1258f93bd952a19
AohigeX <69083318+AohigeX@users.noreply.github.com>
2021-09-25 22:36:02 +0100

    Cambios en código
    Se controla que no se introduzcan números superiores a 100. Añadido un método para invocar una alerta de error de número superior. Código formateado.

--- src/main/java/dad/rubenpablo/adivinapp/AdivinApp.java
+++ src/main/java/dad/rubenpablo/adivinapp/AdivinApp.java
@@ -12,84 +12,93 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class AdivinApp extends Application{
-	
+public class AdivinApp extends Application {
+
 	private Label etiqueta;
 	private TextField inputText;
 	private Button boton;
 	private VBox caja;
 	private int intentos;
 	private int aleatorio;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// Generamos el número aleatorio
 		aleatorio = genAleatorio(); // Rango de 1-100
 		// Label
 		etiqueta = new Label("Introduce un número del 1 al 100");
-		
+
 		// Textfield
 		inputText = new TextField();
 		inputText.setPromptText("Introduce un número");
 		inputText.setMaxWidth(150);
-		
+
 		// Botón
 		boton = new Button();
 		boton.setText("Adivinar");
 		boton.setOnAction(e -> onAdivinarAction(e));
 		boton.setDefaultButton(true);
-		
+
 		// Caja (contenedor)
 		caja = new VBox();
 		caja.setSpacing(15);
 		caja.setAlignment(Pos.CENTER);
 		caja.getChildren().addAll(etiqueta, inputText, boton);
-		
+
 		// Escena
 		Scene escena = new Scene(caja, 320, 200);
-		
+
 		// Configuramos la ventana
 		primaryStage.setTitle("AdivinApp");
 		primaryStage.setScene(escena);
 		primaryStage.show();
 	}
-	
+
 	private int genAleatorio() {
-		return (int) (Math.random()*100 + 1);
+		return (int) (Math.random() * 100 + 1);
 	}
-	
+
+	private Alert numberLimitError() {
+		Alert alerta = new Alert(AlertType.ERROR);
+		alerta.setHeaderText("Error - Número superior al límite");
+		alerta.setContentText("Has introducido un número superior a 100");
+		return alerta;
+	}
+
 	private void onAdivinarAction(ActionEvent e) {
-		 
+
 		try {
 			int recogido = Integer.parseInt(inputText.getText());
-			
-			if (recogido == aleatorio) {
+			if (recogido > 100) {
+				numberLimitError().showAndWait();
+			} else if (recogido == aleatorio) {
 				Alert alerta = new Alert(AlertType.INFORMATION);
 				alerta.setHeaderText("¡Has ganado!");
 				alerta.setContentText("Sólo has necesitado " + intentos + " intentos.\nVuelve a jugar y hazlo mejor.");
 				alerta.showAndWait();
 				aleatorio = genAleatorio(); // Generamos nuevo nº aleatorio
-				intentos=0;
+				intentos = 0;
 			} else {
 				intentos++;
-				String msg = (aleatorio < recogido) ? String.format("El número a adivinar es menor que %d.%nVuelve a intentarlo.", recogido):String.format("El número a adivinar es mayor que %d.%nVuelve a intentarlo.", recogido);
+				String msg = (aleatorio < recogido)
+						? String.format("El número a adivinar es menor que %d.%nVuelve a intentarlo.", recogido)
+						: String.format("El número a adivinar es mayor que %d.%nVuelve a intentarlo.", recogido);
 				Alert alerta = new Alert(AlertType.WARNING);
 				alerta.setHeaderText("¡Has fallado!");
 				alerta.setContentText(msg);
 				alerta.showAndWait();
 			}
 		} catch (NumberFormatException e1) {
-			// En caso de no haber introducido nada o introducir texto, devolver Alert tipo Error
+			// En caso de no haber introducido nada o introducir texto, devolver Alert tipo error
 			Alert alerta = new Alert(AlertType.ERROR);
 			alerta.setHeaderText("AdivinApp - Error");
 			alerta.setContentText("El número introducido no es válido.");
 			alerta.showAndWait();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
+
 }
